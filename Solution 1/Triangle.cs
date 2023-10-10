1c63342700d7a68ab4cd8351396d08e32edfd9e2
AleksandrMatskevich <kokoseke06@gmail.com>
2023-10-10 21:53:09 +0300

    Задание 1

--- Solution 1/Triangle.cs
+++ Solution 1/Triangle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Solution_1
+﻿namespace Solution_1
 {
     using System;
 
